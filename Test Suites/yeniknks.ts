f55305e49a1f0499181bf93942601cb361caf17d
alp <alp@Alp>
2024-02-08 12:49:39 +0300

    sasa

---
+++ Test Suites/yeniknks.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>yeniknks</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7ed06c12-24e1-4e9c-9937-6dc4af6b4a7b</testSuiteGuid>
+   <testCaseLink>
+      <guid>902202f1-f1c8-4fd6-a072-8dee5a71da1d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/DataDrivenTestDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0b66ffa1-99d1-45c0-a453-cefa5f479c02</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>82192979-8fd4-47e4-8075-1e5587d21d67</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
